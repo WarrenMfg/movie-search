3aa2bb3f58f692a081c927230c08a3322c67916b
WarrenMfg <warrenmfg@icloud.com>
2020-06-27 16:11:21 -0400

    sms and email

--- components/Movie.js
+++ components/Movie.js
@@ -1,17 +1,62 @@
 import React from 'react';
+import $ from 'jquery';
 import './css/MovieCSS.css';
 
 export default function Movie({ result }) {
+  const ua = navigator.userAgent;
+
+  const sms = e => {
+    const target = $(e.target);
+    const href = target.attr('href');
+    let smsBody = `Title: ${result.title}%0aRelease Date: ${result.release_date}%0aAverage Vote: ${result.vote_average}%0aOverview: ${result.overview}`;
+
+    target.attr('href', ua.includes('iPhone') ? `${href}&body=${smsBody}` : `${href}+&body=${smsBody}`);
+
+    setTimeout(
+      () => {
+        target.attr('href', 'sms:');
+      },
+      0,
+      target
+    );
+  };
+
+  const email = e => {
+    const target = $(e.target);
+    const href = target.attr('href');
+    let emailBody = `Title: ${result.title}%0aRelease Date: ${result.release_date}%0aAverage Vote: ${result.vote_average}%0aOverview: ${result.overview}`;
+
+    target.attr('href', `${href}${emailBody}`);
+
+    setTimeout(
+      () => {
+        target.attr('href', 'mailto:?subject=Movie Search&body=');
+      },
+      0,
+      target
+    );
+  };
+
   return (
     <div className='card'>
-      <img src={`https://image.tmdb.org/t/p/w185/${result.poster_path}`} alt={result.title} className='card--image' />
+      <div className='card--image-social'>
+        <img src={`https://image.tmdb.org/t/p/w185/${result.poster_path}`} alt={result.title} className='card--image' />
+        <div className='card--social'>
+          <a className='sms' href='sms:' onClick={sms}>
+            SMS
+          </a>
+          <a className='email' href='mailto:?subject=Movie Search&body=' onClick={email}>
+            Email
+          </a>
+        </div>
+      </div>
       <div className='card--content'>
         <h3 className='card--title'>{result.title}</h3>
         <p>
           <small>RELEASE DATE: {result.release_date}</small>
         </p>
         <p>
-          <small>RATING: {result.vote_average}</small>
+          <small>AVERAGE VOTE: {result.vote_average}</small>
         </p>
         <p className='card--desc'>{result.overview}</p>
       </div>
