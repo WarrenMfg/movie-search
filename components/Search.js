889efb7bda307302c39df85d3997bb9ce6188277
WarrenMfg <warrenmfg@icloud.com>
2020-06-27 12:16:23 -0400

    makes fetches to movie db

--- components/Search.js
+++ components/Search.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import $ from 'jquery';
+import { token } from '../config';
+import { handleErrors } from '../utils';
 import './css/SearchCSS.css';
 
 export default function Search() {
   const handleSearch = async e => {
+    // prevent default form behavior
     e.preventDefault();
+    // get user input from form
     const userInput = $('#search');
-    console.log('userInput', userInput.val());
+    // if no input, return
+    if (!userInput.val()) return;
+
+    // fetch data from movie database
+    await fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${userInput.val()}&language=en-US&page=1&include_adult=false`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+      .then(handleErrors)
+      .then(console.log)
+      .catch(console.error);
 
     userInput.val('');
   };
